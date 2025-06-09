86319f87d52090cb79829bacbd4410ffa991c340
ManojM-656565 <137311406+ManojM-656565@users.noreply.github.com>
2025-06-09 14:11:52 +0530

    Basics of Golang

---
+++ Golang-Basics/basics/variables.go
@@ -0,0 +1,25 @@
+package basics
+
+import "fmt"
+
+func variables(){
+	var age int
+    var name1 string ="manoj"
+	var name2 ="murugan"
+
+	count :=10
+	values:=45
+
+	fmt.Println(age)
+	fmt.Println(name1)
+	fmt.Println(name2)
+	fmt.Println(count)
+	fmt.Println(values)
+
+	//default values
+	// numeric =0
+	// bool=false
+	// string =""
+	// pointers,slices,maps,functions and structs :nil
+
+}
